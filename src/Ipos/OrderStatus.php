8e7f09165ac723e1b03b15b959e58fce126c2fb2
Jesper Bisgaard <jesper@adapt.dk>
2018-04-13 15:20:04 -0400

    OHSD-763 Updated status and categories.

--- src/Ipos/OrderStatus.php
+++ src/Ipos/OrderStatus.php
@@ -5,54 +5,74 @@ namespace Interflora\Ipos;
 class OrderStatus
 {
 
-  /**
-   * Order status new
-   */
-  public const NEW = 'new';
+    /**
+     * Order status new
+     */
+    public const NEW = 'new';
 
-  /**
-   * Order status printed
-   */
-  public const PRINTED = 'printed';
+    /**
+     * Order status printed
+     */
+    public const PRINTED = 'printed';
 
-  /**
-   * Order status not printed
-   */
-  public const NOT_PRINTED = 'not_printed';
+    /**
+     * Order status not printed
+     */
+    public const NOT_PRINTED = 'not_printed';
 
-  /**
-   * Order status delivered
-   */
-  public const DELIVERED = 'delivered';
+    /**
+     * Order status delivered
+     */
+    public const DELIVERED = 'delivered';
 
-  /**
-   * Order status canceled
-   */
-  public const CANCELED = 'canceled';
+    /**
+     * Order status canceled
+     */
+    public const CANCELED = 'canceled';
 
-  /**
-   * Order status completed
-   */
-  public const COMPLETED = 'completed';
+    /**
+     * Order status
+     */
+    public const NOT_DELIVERED = 'not_delivered';
 
-  /**
-   * Order status completed
-   */
-  public const NAVISION_ERROR = 'navision_error';
+    /**
+     * Order status paid
+     */
+    public const PAID = 'paid';
 
-  /**
-   * International order status pending approval
-   */
-  public const PENDING_APPROVAL = 'pending_approval';
+    /**
+     * Order status payment failed
+     */
+    public const PAYMENT_FAILED = 'payment_failed';
 
-  /**
-   * International order status outgoing
-   */
-  public const OUTGOING = 'outgoing';
+    /**
+     * Order status navision error
+     */
+    public const SYNC_ERROR = 'sync_error';
 
-  /**
-   * International order status sent to florist gate
-   */
-  public const SENT = 'sent';
+    /**
+     * Order status credited
+     */
+    public const CREDITED = 'credited';
+
+    /**
+     * Order status completed
+     */
+    public const COMPLETED = 'completed';
+
+    /**
+     * International order status pending approval
+     */
+    public const PENDING_APPROVAL = 'pending_approval';
+
+    /**
+     * International order status outgoing
+     */
+    public const OUTGOING = 'outgoing';
+
+    /**
+     * International order status sent to florist gate
+     */
+    public const SENT = 'sent';
 
 }
