615aaa6d7888935f18df7d1610b3637cdb2475ad
Jesper Bisgaard <jesper@adapt.dk>
2018-05-10 10:06:42 -0400

    OHSD-284 Added credit note path and update function.

--- src/Ipos/Api.php
+++ src/Ipos/Api.php
@@ -12,100 +12,101 @@ use GuzzleHttp\Client;
 class Api
 {
 
-  /**
-   * The http client.
-   *
-   * @var mixed
-   */
-  private $client;
+    /**
+     * The http client.
+     *
+     * @var mixed
+     */
+    private $client;
 
-  /**
-   * Authentication service username.
-   *
-   * @var string
-   */
-  private $username = '';
+    /**
+     * Authentication service username.
+     *
+     * @var string
+     */
+    private $username = '';
 
-  /**
-   * Authentication service password.
-   *
-   * @var string
-   */
-  private $password = '';
+    /**
+     * Authentication service password.
+     *
+     * @var string
+     */
+    private $password = '';
 
-  /**
-   * Authentication service uri.
-   *
-   * @var string
-   */
-  private $baseAuthenticationUri;
+    /**
+     * Authentication service uri.
+     *
+     * @var string
+     */
+    private $baseAuthenticationUri;
 
-  /**
-   * IposApi constructor.
-   */
-  public function __construct($url, $authenticationUrl, $username = '', $password = '') {
-    if ($username && $password) {
-      $this->username = $username;
-      $this->password = $password;
-    }
-    $this->baseAuthenticationUri = $authenticationUrl;
+    /**
+     * IposApi constructor.
+     */
+    public function __construct($url, $authenticationUrl, $username = '', $password = '')
+    {
+        if ($username && $password) {
+            $this->username = $username;
+            $this->password = $password;
+        }
+        $this->baseAuthenticationUri = $authenticationUrl;
 
-    $clientArgs = [
-      'base_uri' => $url,
-    ];
-    $this->client = new Client($clientArgs);
-  }
-
-
-  /**
-   * Get the headers for a request.
-   *
-   * @return array
-   *   An array with relevant header information.
-   */
-  protected function getHeaders() {
-    $headers = [
-      'Accept' => 'application/json',
-      'Content-Type'  => 'application/json; charset=UTF-8',
-    ];
-    if ($this->username && $this->password) {
-      $headers['Authorization'] = sprintf('Bearer %s', $this->getToken());
+        $clientArgs   = [
+            'base_uri' => $url,
+        ];
+        $this->client = new Client($clientArgs);
     }
 
-    return $headers;
-  }
+    /**
+     * Get the headers for a request.
+     *
+     * @return array
+     *   An array with relevant header information.
+     */
+    protected function getHeaders()
+    {
+        $headers = [
+            'Accept'       => 'application/json',
+            'Content-Type' => 'application/json; charset=UTF-8',
+        ];
+        if ($this->username && $this->password) {
+            $headers['Authorization'] = sprintf('Bearer %s', $this->getToken());
+        }
 
-  /**
-   * Send order data.
-   *
-   * @param string $data
-   *   The transformed shipment data.
-   * @param string $path
-   *   Path to the endpoint.
-   *
-   * @return array
-   *   Success.
-   */
-  public function sendOrder($data, $path = ApiPath::ORDER_PATH) {
-    try {
-      $response = $this->client->post($this->baseUri . $path, [
-        'json' => $data,
-        'headers' => $this->getHeaders(),
-        'debug' => true,
-      ]);
+        return $headers;
+    }
 
-      return [
-        'code' => 200,
-        'body' => $response->getBody(),
-      ];
+    /**
+     * Send order data.
+     *
+     * @param string $data
+     *   The transformed shipment data.
+     * @param string $path
+     *   Path to the endpoint.
+     *
+     * @return array
+     *   Success.
+     */
+    public function sendOrder($data, $path = ApiPath::ORDER_PATH)
+    {
+        try {
+            $response = $this->client->post($this->baseUri . $path, [
+                'json'    => $data,
+                'headers' => $this->getHeaders(),
+                'debug'   => true,
+            ]);
+
+            return [
+                'code' => 200,
+                'body' => $response->getBody(),
+            ];
+        } catch (\Exception $e) {
+            return [
+                'code' => $e->getCode(),
+                'body' => $e->getMessage(),
+            ];
+        }
     }
-    catch (\Exception $e) {
-      return [
-        'code' => $e->getCode(),
-        'body' => $e->getMessage(),
-      ];
-    }
-  }
 
     /**
      * Send order data with a put request.
@@ -118,20 +119,20 @@ class Api
      * @return array
      *   Success.
      */
-    public function putOrder($data, $path = ApiPath::ORDER_PATH) {
+    public function putOrder($data, $path = ApiPath::ORDER_PATH)
+    {
         try {
             $response = $this->client->put($this->baseUri . $path, [
-                'json' => $data,
+                'json'    => $data,
                 'headers' => $this->getHeaders(),
-                'debug' => true,
+                'debug'   => true,
             ]);
 
             return [
                 'code' => 200,
                 'body' => $response->getBody(),
             ];
-        }
-        catch (\Exception $e) {
+        } catch (\Exception $e) {
             return [
                 'code' => $e->getCode(),
                 'body' => $e->getMessage(),
@@ -139,62 +140,97 @@ class Api
         }
     }
 
-  /**
-   * Update order state for existing order
-   *
-   * @param $orderId
-   * @param $state
-   * @param string $path
-   * @return array
-   */
-  public function updateOrderState($orderId, $state, $path = ApiPath::ORDER_STATE_UPDATE_PATH) {
-    try {
-      $response = $this->client->put($path . '/' . $state, [
-        'json' => $orderId,
-        'headers' => $this->getHeaders(),
-        'debug' => true,
-      ]);
+    /**
+     * Send order data with a put request.
+     *
+     * @param string $data
+     *   The transformed shipment data.
+     * @param string $path
+     *   Path to the endpoint.
+     *
+     * @return array
+     *   Success.
+     */
+    public function putCreditNote($data, $path = ApiPath::CREDIT_NOTE_PATH)
+    {
+        try {
+            $response = $this->client->put($this->baseUri . $path, [
+                'json'    => $data,
+                'headers' => $this->getHeaders(),
+                'debug'   => true,
+            ]);
 
-      return [
-        'code' => 200,
-        'body' => $response->getBody(),
-      ];
-
+            return [
+                'code' => 200,
+                'body' => $response->getBody(),
+            ];
+        } catch (\Exception $e) {
+            return [
+                'code' => $e->getCode(),
+                'body' => $e->getMessage(),
+            ];
+        }
     }
-    catch (\Exception $e) {
-      return [
-        'code' => $e->getCode(),
-        'body' => $e->getMessage(),
-      ];
+
+    /**
+     * Update order state for existing order
+     *
+     * @param        $orderId
+     * @param        $state
+     * @param string $path
+     * @return array
+     */
+    public function updateOrderState($orderId, $state, $path = ApiPath::ORDER_STATE_UPDATE_PATH)
+    {
+        try {
+            $response = $this->client->put($path . '/' . $state, [
+                'json'    => $orderId,
+                'headers' => $this->getHeaders(),
+                'debug'   => true,
+            ]);
+
+            return [
+                'code' => 200,
+                'body' => $response->getBody(),
+            ];
+
+        } catch (\Exception $e) {
+            return [
+                'code' => $e->getCode(),
+                'body' => $e->getMessage(),
+            ];
+        }
     }
-  }
 
-  /**
-   * Send order to queue endpoint.
-   *
-   * @param string $data
-   *   The transformed shipment data.
-   */
-  public function sendOrderQueue($data) {
-    return $this->sendOrder($data, ApiPath::ORDER_QUEUE_PATH);
-  }
+    /**
+     * Send order to queue endpoint.
+     *
+     * @param string $data
+     *   The transformed shipment data.
+     */
+    public function sendOrderQueue($data)
+    {
+        return $this->sendOrder($data, ApiPath::ORDER_QUEUE_PATH);
+    }
 
-  /**
-   * Get authorization token.
-   *
-   * @return string
-   *    Token.
-   */
-  public function getToken() {
-    $credentials = [
-      'form_params' => [
-        '_username' => $this->username,
-        '_password' => $this->password
-      ]
-    ];
-    $client = new Client(['base_uri' => $this->baseAuthenticationUri]);
-    $response = $client->post(ApiPath::LOGIN_PATH, $credentials);
-    return json_decode($response->getBody())->token;
-  }
+    /**
+     * Get authorization token.
+     *
+     * @return string
+     *    Token.
+     */
+    public function getToken()
+    {
+        $credentials = [
+            'form_params' => [
+                '_username' => $this->username,
+                '_password' => $this->password
+            ]
+        ];
+        $client      = new Client(['base_uri' => $this->baseAuthenticationUri]);
+        $response    = $client->post(ApiPath::LOGIN_PATH, $credentials);
+
+        return json_decode($response->getBody())->token;
+    }
 
 }
